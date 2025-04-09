c89580da662cf12617c563860366ff10fc6f120f
akgupta555 <agupta55544@gmail.com>
2025-04-09 16:05:23 +0530

    second

--- client/src/baseUrl.js
+++ client/src/baseUrl.js
@@ -1,3 +1,4 @@
-const baseUrl = "http://localhost:3001";
+const baseUrl =
+  "http://mern-web-app-d3hpefgme9hja3b8.southeastasia-01.azurewebsites.net";
 
 export default baseUrl;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 const port = process.env.PORT || 3001;
 
 const app = express();
@@ -18,6 +19,12 @@ mongoose
 const userSchema = mongoose.Schema({
   name: String,
   lastName: String,
+});
+
+// production script
+app.use(express.static("./client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
 //db model
